d193f6f4460de15e3de1c5f8ef05ceec64ef7f9c
ExoPlant <lennyj2468+github@gmail.com>
2021-11-08 16:48:36 +0000

    Fix creating packs

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 import PySimpleGUI as sg
 import os
+import platform
+import subprocess
 
+root = os.getcwd()
 sg.theme('DarkGrey9')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Button("Make new packwiz pack")],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+new_pack_layout = [  [sg.Text('Pack name:'), sg.InputText()],
+            [sg.Text('Author:'), sg.InputText()],
+            [sg.Text('Pack Version:'), sg.InputText()],
+            [sg.Text('Minecraft Version:'), sg.InputText()],
+            [sg.Text('Modloader (\'forge\' or \'fabric\'):'), sg.InputText()],
+            [sg.Text('Modloader Version:'), sg.InputText()],
+            [sg.Button("Create")]]
+
+pack_layout = [  [sg.Text('Pack name:'), sg.InputText()],
+            [sg.Text('Author:'), sg.InputText()],
+            [sg.Text('Pack Version:'), sg.InputText()],
+            [sg.Text('Minecraft Version:'), sg.InputText()],
+            [sg.Text('Modloader (\'forge\' or \'fabric\'):'), sg.InputText()],
+            [sg.Text('Modloader Version:'), sg.InputText()],
+            [sg.Button("Create")]]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('New Packwiz Pack', new_pack_layout)
+
+def linux_distribution():
+  try:
+    return platform.linux_distribution()
+  except:
+    return ""
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    if event == 'Make new packwiz pack':
-        if os.name() == 'nt':
-            os.system('bin/packwiz.exe init')
-        if os.name() == 'posix':
-            os.system('bin/packwiz init')
+    if event == 'Create':
+        #os.mkdir('instances/{}', str(values[0]))
+        #os.chdir('instances/{}', str(values[0]))
+        if linux_distribution() == '':
+            if platform.system() == 'Windows':
+                name = values[0]
+                author = values[1]
+                pack_version = values[2]
+                mc_version = values[3]
+                modloader = values[4]
+                modloader_version = values[5]
+
+                os.mkdir("instances/" + name)
+                os.chdir("instances/" + name)
+                os.system(root + "/bin/packwiz init " + '--name ' + name + ' --author ' + author + ' --version ' + pack_version + ' --mc-version ' + mc_version + ' --modloader ' + modloader + ' --' + modloader + '-version ' + modloader_version)
+            if platform.system() == 'Darwin':
+                os.system(root + "/bin/packwiz init " + '--name ' + name + ' --author ' + author + ' --version ' + pack_version + ' --mc-version ' + mc_version + ' --modloader ' + modloader + ' --' + modloader + '-version ' + modloader_version)
+    pack_window = sg.Window('Editing Pack', pack_layout)
+    os.chdir('../..')
+            
 
 window.close()
