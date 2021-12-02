3aed4f926de0f2bec40e5762f351cf69901a37ed
ExoPlant <lennyj2468+github@gmail.com>
2021-12-02 16:25:28 +0000

    Completely fix pack listing

--- main.py
+++ main.py
@@ -39,6 +39,13 @@ pack_list = [
             [sg.Text(instances_list)]
             ]
 
+
+if platform.system() == 'Darwin' or 'Linux':
+    linux = True
+    windows = False
+if platform.system() == 'Windows':
+    windows = True
+    linux = False
 # Create the Window
 window = sg.Window("Main Menu", main_menu)
 
@@ -62,8 +69,8 @@ while True:
         modloader = values[4]
         modloader_version = values[5]
 
-        os.mkdir(f"instances/{name}")
-        os.chdir(f"instances/{name}")
+        os.mkdir(f"instances/{name}_pack")
+        os.chdir(f"instances/{name}_pack")
         os.system(f"{packwiz} init --name \"{name}\" --author \"{author}\" --version \"{pack_version}\" --mc-version \"{mc_version}\" --modloader \"{modloader}\" --{modloader}-version \"{modloader_version}\"")
         
         window = sg.Window("Editing Pack", pack_edit)
@@ -78,22 +85,25 @@ while True:
         window = sg.Window("Creating a new pack", pack_create)
         
     if event == "Open an existing pack":
-        if platform.system() == "Windows":
+        if windows == True:
             command = f"cmd.exe dir"
-        elif platform.system() == "Darwin" or "Linux":
-            command = f"ls -Al | awk {'print $9'}"
+            command2 = f""
+        elif linux == True:
+            command = f"ls"
+            #command2 =  f"| awk {'print $9'}"
+            command2 = f"| grep _pack"
 
         #status, output = commands.getstatusoutput(f"{command} {root}/instances")
-        cmd = f"{command} {root}/instances/"
+        cmd = f"{command} {root}/instances/ {command2}"
         process = Popen(
         args=cmd,
         stdout=PIPE,
         stderr=PIPE,
         shell=True)
             
-
+        instances_list = process.stdout.read()[:-6]
         pack_list = [
-                    [sg.Text(process.stdout.read())]
+                    [sg.Text(instances_list.decode("utf-8"))]
                     ]
         print(process.stdout.read())
         window = sg.Window("Listing existing packs", pack_list)
