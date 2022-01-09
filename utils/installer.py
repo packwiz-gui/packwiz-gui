16ccd6168c682d5fb868ead46582a8d2b882296f
Samisafool <thenerdiestguy@gmail.com>
2022-01-09 15:50:44 +0530

    lol

--- utils/installer.py
+++ utils/installer.py
@@ -95,7 +95,7 @@ if not os.path.exists(f"{server_root}"):
     os.makedirs(f"{server_root}")
 os.chdir(pack_root)
 def runserve():
-    subprocess.run([PACKWIZ_BINARY, "serve"])
+    subprocess.run(["cd", pack_root, "&&", PACKWIZ_BINARY, "serve"])
 serve_thread  = threading.Thread(target=runserve)
 serve_thread.start()
 os.chdir(server_root)
