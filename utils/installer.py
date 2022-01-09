fb9b92c15989a802a602043c4d336f4b2340eb81
Samisafool <thenerdiestguy@gmail.com>
2022-01-09 20:56:12 +0530

    Exo why so much bs

--- utils/installer.py
+++ utils/installer.py
@@ -24,33 +24,22 @@ def opentoml(filename):
 
 def main():
     # Just a bunch of setup stuff
-    PLATFORM = None
-    SHELL = None
-    UNIXTYPE = None
-    ROOT = f"{os.getcwd()}"
-    while not os.path.isfile(f"{ROOT}/packwiz_gui.py"):
+    root = f"{os.getcwd()}"
+    while not os.path.isfile(f"{root}/packwiz_gui.py"):
         os.chdir("..")
-        ROOT = os.getcwd()
-    PACKWIZ_BINARY = f"{ROOT}\\bin\\packwiz.exe" if platform.system() == "Windows" else f"{ROOT}/bin/packwiz"
-    INSTANCES_DIR = f"{ROOT}/instances"
+        root = os.getcwd()
+    instances = f"{root}/instances"
     fabric_installer_version = None
     fabric_loader_version = None
     pack_name = None
     minecraft_version = None
     system_ram = None
-    SHELL = None
 
     if platform.system() == "Windows":
-        PLATFORM = "Win"
-        UNIXTYPE = "Non-unix"
-        PYTHON = "python"
-    else:
-        PLATFORM = "Unix"
-        UNIXTYPE = "macOS" if platform.system() == "Darwin" else "Linux"
-        PYTHON = "python3"
-
-    if PLATFORM == "Win":
+        osys = "Win"
         os.system("title Fabric + packwiz installer script")
+    else:
+        osys = "Unix"
 
     print("#######################################")
     print("### FABRIC+PACKWIZ SERVER INSTALLER ###")
@@ -58,11 +47,11 @@ def main():
 
     print("The below prompt will ask you some questions for the setup of the server. The value in [] is the recommended value, if you hit enter without specifying a version it will pass the recommended value.")
 
-    while pack_name in ("", None) or not os.path.isfile(f"{INSTANCES_DIR}/{pack_name}/pack.toml"):
+    while pack_name in ("", None) or not os.path.isfile(f"{instances}/{pack_name}/pack.toml"):
         pack_name = input("Pack name: ")
         if pack_name == "":
             print("Error: Please specify a pack name!")
-        elif not os.path.isfile(f"{INSTANCES_DIR}/{pack_name}/pack.toml"):
+        elif not os.path.isfile(f"{instances}/{pack_name}/pack.toml"):
             print("Error: please enter a valid pack!")
 
     fabric_installer_version = input("Fabric installer version (NOT fabric loader) [0.10.2]: ")
@@ -73,10 +62,9 @@ def main():
     if system_ram == "":
         system_ram = "4096"
 
-    pack_toml = opentoml(f"{INSTANCES_DIR}/{pack_name}/pack.toml")
+    pack_toml = opentoml(f"{instances}/{pack_name}/pack.toml")
 
     minecraft_version = pack_toml["versions"]["minecraft"]
-    fabric_loader_version = pack_toml["versions"]["fabric"]
 
     print("")
     print("Data provided:")
@@ -85,7 +73,7 @@ def main():
     print(f"RAM to allocate: {system_ram}")
     print("")
 
-    pack_root = f"{INSTANCES_DIR}/{pack_name}"
+    pack_root = f"{instances}/{pack_name}"
     server_root = f"{pack_root}/server"
     if not os.path.exists(f"{server_root}"):
         os.makedirs(f"{server_root}")
@@ -108,11 +96,11 @@ def main():
     subprocess.run(["java", "-jar", "packwiz-installer-bootstrap.jar", "http://localhost:8080/pack.toml"], check=True)
 
     startshfile = f"java -Xms{str(int(int(system_ram) / 4))}M -Xmx{system_ram}M -jar fabric-server-launch.jar"
-    with open("start.bat", "a", encoding="UTF-8") as f:
-        f.write(startshfile)
-    with open("start.sh", "a", encoding="UTF-8") as f:
-        f.write(startshfile)
-    if PLATFORM == "Unix":
+    with open("start.bat", "a", encoding="UTF-8") as startscript:
+        startscript.write(startshfile)
+    with open("start.sh", "a", encoding="UTF-8") as startscript:
+        startscript.write(startshfile)
+    if osys == "Unix":
         subprocess.run(["chmod", "+x", "start.sh"], check=True)
 
 if __name__ == "__main__":
